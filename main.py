38c090450669787a104531f988a0b22938098ead
OlankLN <olank996@gmail.com>
2026-06-24 22:47:43 -0300

    connect menu and game classes

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import pygame
+from code.game import Game
 
-print("Setup start")
-pygame.init()
-window = pygame.display.set_mode(size=(600, 480))
-print("Setup end")
-
-print("Loop start")
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
+game = Game()
+game.run()
