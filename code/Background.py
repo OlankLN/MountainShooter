26664459b1aa35e6e192d21c70489fcace155019
OlankLN <olank996@gmail.com>
2026-06-27 19:06:32 -0300

    Inclusao asstes

---
+++ code/Background.py
@@ -0,0 +1,11 @@
+from code.Entity import Entity
+
+class Background(Entity):
+
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
+
+    def move(self, ):
+        self.rect.centerx -= 1
+        if self.rect.right
+        pass
